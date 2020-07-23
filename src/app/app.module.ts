0ecedb5e8bf9bd672886d46fd6a49cbfd09a6885
Nellia Salimova <salimova.nellia@gmail.com>
2020-07-23 12:32:13 +0300

    adding news-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material';
 import {MatButtonModule} from '@angular/material/button';
+import { NewsListComponent } from './news-list/news-list.component';
+import {NewsCardComponent} from './news-list/news-card/news-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsListComponent,
+    NewsCardComponent
   ],
   imports: [
     BrowserModule,

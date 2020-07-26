9dd821283be508877450bc4e0e8e86e10bdff183
Nellia Salimova <salimova.nellia@gmail.com>
2020-07-26 09:33:36 +0300

    fix

--- src/app/services/news-api.service.ts
+++ src/app/services/news-api.service.ts
@@ -13,14 +13,14 @@ export class NewsApiService {
   constructor(private httpClient: HttpClient) { }
 
   public getNews(amount: number = 10, page: number = 1): Observable<Article> {
-    return this.httpClient.get<Article>(`http://newsapi.org/v2/top-headlines?language=en&apiKey=${this.API_KEY}&pageSize=${amount}&page=${page}`)
+    return this.httpClient.get<Article>(`https://newsapi.org/v2/top-headlines?language=en&apiKey=${this.API_KEY}&pageSize=${amount}&page=${page}`)
   }
 
   public getNewsByKeyword(text: string, amount: number = 10, page: number = 1): Observable<Article> {
-    return this.httpClient.get<Article>(`http://newsapi.org/v2/top-headlines?q=${text}&language=en&apiKey=${this.API_KEY}&pageSize=${amount}&page=${page}`)
+    return this.httpClient.get<Article>(`https://newsapi.org/v2/top-headlines?q=${text}&language=en&apiKey=${this.API_KEY}&pageSize=${amount}&page=${page}`)
   }
 
   public getArticleByTitle(selectedTitle: string): Observable<Article> {
-    return this.httpClient.get<Article>(`http://newsapi.org/v2/top-headlines?q=${selectedTitle}&language=en&apiKey=${this.API_KEY}`);
+    return this.httpClient.get<Article>(`https://newsapi.org/v2/top-headlines?q=${selectedTitle}&language=en&apiKey=${this.API_KEY}`);
   }
 }
